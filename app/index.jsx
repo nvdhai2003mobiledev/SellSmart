c14f3d60bf61a117cd2615264afee3c96efe94f8
nvdhai2003mobiledev <nvdhai2003.mobiledev@gmail.com>
2024-11-30 23:33:56 +0700

    test git

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,12 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import Login from "./login";
 
 const Index = () => {
   const router = useRouter();
   return (
-    <View>
-      <Text>index</Text>
-      <Button title="Splash" onPress={() => router.push("splash")} />
-    </View>
+    <Login />
   );
 };
 
